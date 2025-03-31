d37df42227d16b799b60c88fa591768e4b81034b
SILENT-GUO <guozebin2324@outlook.com>
2025-03-31 00:05:49 -0400

    test

--- src/network/udp.cpp
+++ src/network/udp.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "udp.hpp"
+
+#include <unistd.h>
+
+namespace paxosdb {
+
+UDPRecvMessage::UDPRecvMessage(DFNetwork *network): _network(network), _iSocketFD(-1) {
+
+}
+
+UDPRecvMessage::~UDPRecvMessage() {
+    if (_iSocketFD != -1) {
+        // close socket
+        close(_iSocketFD);
+        _iSocketFD = -1;
+    }
+}
+
+void UDPRecvMessage::stop() {
+    
+}
+
+}
